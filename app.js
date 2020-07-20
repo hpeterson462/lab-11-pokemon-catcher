c6008c3f8f8ae3a1757638443669f80e701bf007
Heather Peterson <hpeterson462@gmail.com>
2020-07-20 14:20:01 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+// import functions and grab DOM elements
+import from '../rawData';
+import { getRandomPoke } from './';
+// initialize state
+let
+// set event listeners to update state and DOM
+
+//we need correct pick - doesn't matter which one as long as it hasn't been picked
+//decide correct answer
+
+const randomOneOrZero = Math.round(Math.random());
+
+let correctAnswer = null;
+
+if (randomOneOrZero) {
+    correctAnswer = randomPoke1;
+} else {
+    correctAnswer = randomPoke2;
+}
+
+//grab the array of labels
+
+//get first label
+
+//get input in the first label
+
+//get img in first label
